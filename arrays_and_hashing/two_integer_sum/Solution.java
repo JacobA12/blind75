2b7ccb2bde9ef40f04c0dca48362a4389ebf36ed
JacobA12 <jacob.adams121202@gmail.com>
2024-11-07 14:40:58 -0800

    hashmap two pass solution- passes all tests

--- arrays_and_hashing/two_integer_sum/Solution.java
+++ arrays_and_hashing/two_integer_sum/Solution.java
@@ -1,18 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
 
   public static int[] twoSum(int[] nums, int target) {
+    Map<Integer, Integer> numMap = new HashMap<>();
+
     for (int i = 0; i < nums.length; i++) {
-      for (int j = i + 1; j < nums.length; i++) {
-        if (nums[i] + nums[j] == target && i != j) {
-          return new int[]{i,j};
-        }
+      numMap.put(nums[i], i);
+    }
+
+    for (int i = 0; i < nums.length; i++) {
+      int diff = target - nums[i];
+      if (numMap.containsKey(diff) && numMap.get(diff) != i) {
+        return new int[] {i, numMap.get(diff)};
       }
     }
-    return null;
+
+    return new int[]{0};
   }
 
   public static void main(String[] args) {
-    int[] nums = {5, 5};
+    int[] nums = {2, 3, 5, 5};
     int target = 10;
     int[] solution = twoSum(nums, target);
     for (int i : solution) {
