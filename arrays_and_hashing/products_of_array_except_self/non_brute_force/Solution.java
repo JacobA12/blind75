6a22303485168f46bf9417524269377a848a37ae
Jacob Adams big-boi <jacob.adams121202@gmail.com>
2024-11-25 23:47:23 -0800

    new branch

--- arrays_and_hashing/products_of_array_except_self/non_brute_force/Solution.java
+++ arrays_and_hashing/products_of_array_except_self/non_brute_force/Solution.java
@@ -2,6 +2,14 @@ package non_brute_force;
 
 public class Solution {
   public int[] productExceptSelf(int[] nums) {
-          
+    int currIndex = 0;
+    int[] sol = new int[nums.length];
+    int[] prefix = new int[nums.length - 1];
+    int[] suffix = new int[nums.length - 1];
+    for (int i = 0; i < nums.length; i++) {
+      if (i != currIndex) {
+        prefix[i] = nums[i];
+      }
+    }
   }
 }
