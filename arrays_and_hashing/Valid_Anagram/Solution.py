0e2713dd41fc9189fdd231498496524217dd6777
Jacob Adams big-boi <jacob.adams121202@gmail.com>
2024-11-26 00:13:50 -0800

    HashTable solution

--- arrays_and_hashing/Valid_Anagram/Solution.py
+++ arrays_and_hashing/Valid_Anagram/Solution.py
@@ -1,15 +1,23 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s_sorted = ''.join(sorted(s))
-        t_sorted = ''.join(sorted(t))
-        
-        if s_sorted == t_sorted:
-          return True
-        else:
+        if len(s) != len(t):
           return False
         
-s = "racecar"
-t = "carrace"
+        countS, countT = {}, {}
+
+        for i in range(len(s)):
+          countS[s[i]] = 1 + countS.get(s[i], 0)
+          countT[t[i]] = 1 + countT.get(t[i], 0)
+        for c in countS:
+          if countS[c] != countT.get(c, 0):
+            return False
+        
+        return True
+
+        
+        
+s = 'racecar'
+t = 'carrace'
 
 s2 = 'jar'
 t2 = 'jam'
